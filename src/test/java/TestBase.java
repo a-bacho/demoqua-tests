1184d0acb4423ee5fd284958610a610fe6095eb3
andrew <andrew.bacho@outlook.com>
2021-12-25 19:29:57 +0300

    .steps update

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,11 +1,15 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import pages.RegistrationsPage;
-
 public class TestBase {
     RegistrationsPage registrationsPage = new RegistrationsPage();
 
     @BeforeAll
-    static void beforeAll() {Configuration.startMaximized = true;}
+    static void beforeAll() {
+        Configuration.startMaximized = true;
 
+
+        System.setProperty("selenide.browser", "edge");
+    }
 }
+
---
+++ src/test/java/io/github/qaguru/allure/StepLambdaTest.java
@@ -0,0 +1,45 @@
+package io.github.qaguru.allure;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class StepLambdaTest {
+  public final String REPOSITORY = "eroshenkoam/allure-example";
+
+    @Test
+
+    public void testGithub() {
+        System.setProperty("selenide.browser", "firefox");
+
+        step("Открываем главную страницу", () -> {
+                    open("https://github.com");
+                });
+        step("Ищем репозиторий" + REPOSITORY, () -> {
+
+
+                    $(".header-search-input").click();
+                    $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+                    $(".header-search-input").submit();
+                });
+        step("Переходим в репозиторий" + REPOSITORY, () -> {
+
+                    $(linkText("eroshenkoam/allure-example")).click();
+                });
+        step("Открываем таб issues");
+        $(partialLinkText("Issues")).click();
+        $(withText("#68")).should(Condition.visible);
+
+
+    }
+
+}
+
+
+
